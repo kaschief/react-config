930856a93c3b33458c31cff097ed2cbb2f0a85dc
Kaschief Johnson <kaschiefj@gmail.com>
2021-10-06 18:49:36 +0200

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/main.less';
+
 class Welcome extends React.Component {
   render() {
     return <h1>Hello World from React boilerplate</h1>;
